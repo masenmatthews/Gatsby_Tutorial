a42821f255a2088bc73d1a8d4b4ef12f4685d254
Masen Matthews <masenmatthews@gmail.com>
2018-11-21 14:06:14 -0800

    complete tutorial (yay)

--- tutorial-part-four/gatsby-config.js
+++ tutorial-part-four/gatsby-config.js
@@ -4,14 +4,27 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "minimal-ui",
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
         path: `${__dirname}/src/`,
       },
     },
+    'gatsby-plugin-offline',
     `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
+    `gatsby-plugin-react-helmet`
     {
       resolve: `gatsby-plugin-typography`,
       options: {
