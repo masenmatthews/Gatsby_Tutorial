9c11c5c13110ff985535f84e70748817581ed7c3
Masen Matthews <masenmatthews@gmail.com>
2018-11-21 11:20:03 -0800

    finish tutorial part 7

--- tutorial-part-four/src/pages/index.js
+++ tutorial-part-four/src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { graphql } from "gatsby"
 import { css } from "react-emotion"
+import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
 export default ({ data }) => {
-  console.log(data)
   return (
     <Layout>
       <div>
@@ -17,33 +16,40 @@ export default ({ data }) => {
         >
           Amazing Pandas Eating Things
         </h1>
-        // blog posts - pulls data from markdown files
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <h3
+            <Link
+              to={node.fields.slug}
               className={css`
-                margin-bottom: ${rhythm(1 / 4)};
-              `}
+                text-decoration: none;
+                color: inherit;
+              `
+}
             >
-              {node.frontmatter.title}{" "}
-              <span
+
+              <h3
                 className={css`
-                  color: #bbb;
+                  margin-bottom: ${rhythm(1 / 4)};
                 `}
               >
-                — {node.frontmatter.date}
-              </span>
-            </h3>
-            <p>{node.excerpt}</p>
+                {node.frontmatter.title}{" "}
+                <span
+                  className={css`
+                    color: #bbb;
+                  `}
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
           </div>
         ))}
       </div>
     </Layout>
   )
 }
-
-// graphql query
 
 export const query = graphql`
   query {
@@ -55,6 +61,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
